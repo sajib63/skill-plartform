93184f43a41c16ac1083f92689e1651577ada6d0
Sajib Ahmed <sajib7315@gmail.com>
2022-10-25 08:27:48 +0600

    adding daysi ui and tailwind

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkR00ifURUs7durmON6ECCSh--SRw0R8o",
+  authDomain: "assignment-10-a2144.firebaseapp.com",
+  projectId: "assignment-10-a2144",
+  storageBucket: "assignment-10-a2144.appspot.com",
+  messagingSenderId: "58134089585",
+  appId: "1:58134089585:web:7c89e01db61616533cf0d0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
